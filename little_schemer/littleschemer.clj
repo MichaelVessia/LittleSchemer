9b3c800494bf2233db2c919ee20be889476b6b72
MichaelVessia <michaelvessia@mail.adelphi.edu>
2015-10-05 23:45:58 -0400

    Includes work on chapter 4

--- little_schemer/littleschemer.clj
+++ little_schemer/littleschemer.clj
@@ -4,6 +4,7 @@
   "Test whether something is not a list"
   [x] (not (list? x)))
 
+
 ;; Chapter 2
 
 (defn lat?
@@ -24,7 +25,6 @@
       false
       (or (member? a (rest lat))))))
 
-
 (defn rember
   "Remove the first occurence of an atom from a lat"
   [a lat]
@@ -42,7 +42,8 @@
     (cons (first (first l)) (firsts (rest l)))))
 
 (defn insertR
-  "Searches for a match with old, if found inserts new directly after it in the given lat"
+  "Searches for a match with old, if found inserts new directly after it in the
+  given lat"
   [new old lat]
   (cond
     (not (lat? lat)) lat
@@ -64,9 +65,9 @@
   [new old lat]
   (cond
     (not (lat? lat)) lat
-  (empty? lat) lat
-  (= old (first lat)) (cons new (rest lat))
-  :else (cons (first lat) (subst new old (rest lat)))))
+    (empty? lat) lat
+    (= old (first lat)) (cons new (rest lat))
+    :else (cons (first lat) (subst new old (rest lat)))))
 
 (defn subst2
   "Replaces either the first occurence of o1 or the first occurence of o2 with new"
@@ -114,4 +115,65 @@
     :else (cons (first lat) (multiSubst new old (rest lat)))))
 
 ;; Chapter 4
-      
+
+(defn plus
+  "Add two numbers"
+  [n m]
+  (if (zero? m)
+    n
+    (inc (plus n (dec m)))))
+
+(defn minus
+  "Subtract two numbers"
+  [n m]
+  (if (zero? m)
+    n
+    (dec (minus n (dec m)))))
+
+(defn addtup
+  "Adds all the numbers in a tuple, which is defined as a list of numbers"
+  [tup]
+  (if (empty? tup)
+    0
+    (plus (first tup) (addtup (rest tup)))))
+
+(defn multiply
+  "Multiplies two numbers"
+  [n m]
+  (if (zero? m)
+    0
+    (plus n (multiply n (dec m)))))
+
+(defn tup+
+  "Adds corresponding elements of two tuples, forming a new tuple of the same
+  length with the value of position i being the sum of the values of the two
+  given tuples' position i.  Tuples must be of the same length"
+  [tup1 tup2]
+  (cond
+    (empty? tup1) tup1
+    (empty? tup2) tup2
+    :else (cons (plus (first tup1) (first tup2))
+                (tup+ (rest tup1) (rest tup2)))))
+
+(defn greater
+  "Returns boolean value indicating whether n > m"
+  [n m]
+  (cond
+    (or (= n m) (zero? n)) false
+    (zero? m) true
+    :else (greater (dec n) (dec m))))
+
+(defn less
+  "Returns boolean value indicating whether n < m"
+  [n m]
+  (cond
+    (or (= n m) (zero? n)) false
+    (zero? n) true
+    :else (less (dec n) (dec m))))
+
+(defn equals
+  "Equality rewrittten using greater and less"
+  [n m]
+  (if
+      (or (greater n m) (less n m)) false
+      true))
